36ec554e9ce69080a44dc46599b7187dd2b69bd0
SokolovskyVadim <sokolovsky.v.a@gmail.com>
2019-08-08 14:39:55 +0300

    new_version_1_2

--- FALCON/js/index.js
+++ FALCON/js/index.js
@@ -1,74 +1,49 @@
 // МОДАЛЬНОЕ ОКНО МОЖНО СОЗДАВАТЬ НЕСКОЛЬКО НА СТРАНИЦЕ!
-!function(e){"function"!=typeof e.matches&&(e.matches=e.msMatchesSelector||e.mozMatchesSelector||e.webkitMatchesSelector||function(e){for(var t=this,o=(t.document||t.ownerDocument).querySelectorAll(e),n=0;o[n]&&o[n]!==t;)++n;return Boolean(o[n])}),"function"!=typeof e.closest&&(e.closest=function(e){for(var t=this;t&&1===t.nodeType;){if(t.matches(e))return t;t=t.parentNode}return null})}(window.Element.prototype);
+   !function(e){"function"!=typeof e.matches&&(e.matches=e.msMatchesSelector||e.mozMatchesSelector||e.webkitMatchesSelector||function(e){for(var t=this,o=(t.document||t.ownerDocument).querySelectorAll(e),n=0;o[n]&&o[n]!==t;)++n;return Boolean(o[n])}),"function"!=typeof e.closest&&(e.closest=function(e){for(var t=this;t&&1===t.nodeType;){if(t.matches(e))return t;t=t.parentNode}return null})}(window.Element.prototype);
 
-
-document.addEventListener('DOMContentLoaded', function() {
-
-   /* Записываем в переменные массив элементов-кнопок и подложку.
-      Подложке зададим id, чтобы не влиять на другие элементы с классом overlay*/
-   var modalButtons = document.querySelectorAll('.js-open-modal'),
-       overlay      = document.querySelector('.js-overlay-modal'),
-       closeButtons = document.querySelectorAll('.js-modal-close');
-
-
-   /* Перебираем массив кнопок */
-   modalButtons.forEach(function(item){
-
-      /* Назначаем каждой кнопке обработчик клика */
-      item.addEventListener('click', function(e) {
-
-         /* Предотвращаем стандартное действие элемента. Так как кнопку разные
-            люди могут сделать по-разному. Кто-то сделает ссылку, кто-то кнопку.
-            Нужно подстраховаться. */
-         e.preventDefault();
-
-         /* При каждом клике на кнопку мы будем забирать содержимое атрибута data-modal
-            и будем искать модальное окно с таким же атрибутом. */
-         var modalId = this.getAttribute('data-modal'),
-             modalElem = document.querySelector('.modal[data-modal="' + modalId + '"]');
-
-
-         /* После того как нашли нужное модальное окно, добавим классы
-            подложке и окну чтобы показать их. */
-         modalElem.classList.add('active');
-         overlay.classList.add('active');
-      }); // end click
-
-   }); // end foreach
-
-
-   closeButtons.forEach(function(item){
-
-      item.addEventListener('click', function(e) {
-         var parentModal = this.closest('.modal');
-
-         parentModal.classList.remove('active');
-         overlay.classList.remove('active');
+   document.addEventListener('DOMContentLoaded', function() {
+      /* Записываем в переменные массив элементов-кнопок и подложку.
+         Подложке зададим id, чтобы не влиять на другие элементы с классом overlay*/
+      var modalButtons = document.querySelectorAll('.js-open-modal'),
+         overlay      = document.querySelector('.js-overlay-modal'),
+         closeButtons = document.querySelectorAll('.js-modal-close');
+      /* Перебираем массив кнопок */
+      modalButtons.forEach(function(item){
+         /* Назначаем каждой кнопке обработчик клика */
+         item.addEventListener('click', function(e) {
+            /* Предотвращаем стандартное действие элемента. Так как кнопку разные
+               люди могут сделать по-разному. Кто-то сделает ссылку, кто-то кнопку.
+               Нужно подстраховаться. */
+            e.preventDefault();
+            /* При каждом клике на кнопку мы будем забирать содержимое атрибута data-modal
+               и будем искать модальное окно с таким же атрибутом. */
+            var modalId = this.getAttribute('data-modal'),
+               modalElem = document.querySelector('.modal[data-modal="' + modalId + '"]');
+            /* После того как нашли нужное модальное окно, добавим классы
+               подложке и окну чтобы показать их. */
+            modalElem.classList.add('active');
+            overlay.classList.add('active');
+         }); // end click
+      }); // end foreach
+      closeButtons.forEach(function(item){
+         item.addEventListener('click', function(e) {
+            var parentModal = this.closest('.modal');
+            parentModal.classList.remove('active');
+            overlay.classList.remove('active');
+         });
+      }); // end foreach
+      document.body.addEventListener('keyup', function (e) {
+         var key = e.keyCode;
+         if (key == 27) {
+               document.querySelector('.modal.active').classList.remove('active');
+               document.querySelector('.overlay').classList.remove('active');
+         };
+      }, false);
+      overlay.addEventListener('click', function() {
+         document.querySelector('.modal.active').classList.remove('active');
+         this.classList.remove('active');
       });
-
-   }); // end foreach
-
-
-    document.body.addEventListener('keyup', function (e) {
-        var key = e.keyCode;
-
-        if (key == 27) {
-
-            document.querySelector('.modal.active').classList.remove('active');
-            document.querySelector('.overlay').classList.remove('active');
-        };
-    }, false);
-
-
-    overlay.addEventListener('click', function() {
-        document.querySelector('.modal.active').classList.remove('active');
-        this.classList.remove('active');
-    });
-
-
-
-
-}); // end ready
+   }); // end ready
 // КОНЕЦ МОДАЛЬНОГО ОКНА
 
 //поворачиваюшиеся менюшки
@@ -97,24 +72,27 @@ document.addEventListener('DOMContentLoaded', function() {
 		})
 	});
 })();
+//поворачиваюшиеся менюшки(конец)
+
 
 // ХЭДЕР
-		$(document).ready(function(){
-			// Фикмированная шапка при скролле
-			$(window).scroll(function(){
-				if ($(this).scrollTop() > 150) {
-					$('.header_bottom_2').css('display','block');
-				} else {
-					$('.header_bottom_2').css('display','none');
-				};
-			});
-		});
+$(document).ready(function(){
+   // Фикмированная шапка при скролле
+   $(window).scroll(function(){
+      if ($(this).scrollTop() >= 150) {
+         $('.header_bottom_2').fadeIn("slow")
+      } else {
+         $('.header_bottom_2').fadeOut("slow");
+      };
+   });
+});
 
+// ХЭДЕР(конец)
+
+//кнопка подъема вверх
       $(document).ready(function(){
          $('body').append('<a href="#" id="go-top" title="Вверх">&#9650;</a>');
        });
-
-//кнопка подъема вверх
        $(function() {
         $.fn.scrollToTop = function() {
          $(this).hide().removeAttr("href");
@@ -133,18 +111,30 @@ document.addEventListener('DOMContentLoaded', function() {
        $(function() {
         $("#go-top").scrollToTop();
        });
-// мобильное меню
+//кнопка подъема вверх(конец)
 
+
+// мобильное меню
 $('.menu-btn').on('click', function(e) {
    e.preventDefault();
    $('.mobile_menu_in').toggleClass('menu-active');
    $('.content').toggleClass('content-active');
-})
+      if ($(".mobile_menu_in").hasClass('menu-active')) { // начиная с этой строки я сделал затемнение заднего экрана
+      $('<div class="overlay_mobile">&nbsp;</div>').prependTo($('body'));
+      } else {
+      $('.overlay_mobile').remove();
+}
+});
 
-//end of right menu
+// мобильное меню (конец)
 
+// анимированная мобильная кнопка
 $(document).ready(function(){
   $(".hamburger").click(function(){
      $(this).toggleClass("open");
   });
 });
+// анимированная мобильная кнопка (конец)
+
+
+
